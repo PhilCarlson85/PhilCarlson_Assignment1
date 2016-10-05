db4c7bea04e7d840c0bd8d253716305bcef0d425
PhilCarlson85 <philcarlson85@gmail.com>
2016-10-04 19:30:07 -0500

    Add files via upload

---
+++ Primes.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Phil on 10/4/16.
+ */
+function isPrime(value) {
+    for(var i = 2; i < value; i++) {
+        if(value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
+}
