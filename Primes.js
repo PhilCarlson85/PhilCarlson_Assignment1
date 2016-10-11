97b9afc9ee43402e6bf226a5a2c36e1b5fcc6d5d
PhilCarlson85 <philcarlson85@gmail.com>
2016-10-10 20:57:34 -0500

    Add files via upload

--- Primes.js
+++ Primes.js
@@ -1,11 +1,19 @@
-/**
- * Created by Phil on 10/4/16.
- */
 function isPrime(value) {
-    for(var i = 2; i < value; i++) {
+    for(var i = 2; i < (value/2 + 1); i++) {
         if(value % i === 0) {
             return false;
         }
     }
     return value > 1;
+}
+
+var count = 1;
+var numberOfPrimes = 0;
+while (numberOfPrimes < 10001) {
+    count++;
+    if(isPrime(count)){
+      numberOfPrimes++;
+    }
+
+    console.log(count, isPrime(count), numberOfPrimes);
 }
